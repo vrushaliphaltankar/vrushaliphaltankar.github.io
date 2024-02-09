11f7c71960fd2ce4afa257a8e4700f3542795dfb
phaltankarvrushali <phaltankarvrushali28@gmail.com>
2024-02-08 21:09:51 -0500

    Added portfolio

---
+++ data/skillsData.js
@@ -0,0 +1,19 @@
+export const skillsData = [
+    'Java',
+    'Spring Boot',
+    'Docker',
+    'HTML',
+    'Javascript',
+    'C++',
+    'Python',
+    'CSS',
+    'React',
+    'Kubernetes',
+    'NodeJS',
+    'Go',
+    'Linux',
+    'Ubuntu',
+    'MongoDB',
+    'MySQL',
+    'PostgreSQL'
+]
